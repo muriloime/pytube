295bab04188e172e3fa3880d17caa0d73d8a7ded
Murilo Vasconcelos <muriloime@gmail.com>
2023-01-25 07:59:27 -0300

    fix error

--- pytube/contrib/channel.py
+++ pytube/contrib/channel.py
@@ -11,287 +11,288 @@ logger = logging.getLogger(__name__)
 
 
 class Channel(Playlist):
-    def __init__(self, url: str, proxies: Optional[Dict[str, str]] = None):
-        """Construct a :class:`Channel <Channel>`.
+  def __init__(self, url: str, proxies: Optional[Dict[str, str]] = None):
+    """Construct a :class:`Channel <Channel>`.
 
-        :param str url:
-            A valid YouTube channel URL.
-        :param proxies:
-            (Optional) A dictionary of proxies to use for web requests.
-        """
-        super().__init__(url, proxies)
+    :param str url:
+        A valid YouTube channel URL.
+    :param proxies:
+        (Optional) A dictionary of proxies to use for web requests.
+    """
+    super().__init__(url, proxies)
 
-        self.channel_uri = extract.channel_name(url)
+    self.channel_uri = extract.channel_name(url)
 
-        self.channel_url = (
-            f"https://www.youtube.com{self.channel_uri}"
-        )
+    self.channel_url = (
+        f"https://www.youtube.com{self.channel_uri}"
+    )
 
-        self.videos_url = self.channel_url + '/videos'
-        self.playlists_url = self.channel_url + '/playlists'
-        self.community_url = self.channel_url + '/community'
-        self.featured_channels_url = self.channel_url + '/channels'
-        self.about_url = self.channel_url + '/about'
+    self.videos_url = self.channel_url + '/videos'
+    self.playlists_url = self.channel_url + '/playlists'
+    self.community_url = self.channel_url + '/community'
+    self.featured_channels_url = self.channel_url + '/channels'
+    self.about_url = self.channel_url + '/about'
 
-        # Possible future additions
-        self._playlists_html = None
-        self._community_html = None
-        self._featured_channels_html = None
-        self._about_html = None
+    # Possible future additions
+    self._playlists_html = None
+    self._community_html = None
+    self._featured_channels_html = None
+    self._about_html = None
 
-    @property
-    def channel_name(self):
-        """Get the name of the YouTube channel.
+  @property
+  def channel_name(self):
+    """Get the name of the YouTube channel.
 
-        :rtype: str
-        """
-        return self.initial_data['metadata']['channelMetadataRenderer']['title']
+    :rtype: str
+    """
+    return self.initial_data['metadata']['channelMetadataRenderer']['title']
 
-    @property
-    def channel_id(self):
-        """Get the ID of the YouTube channel.
+  @property
+  def channel_id(self):
+    """Get the ID of the YouTube channel.
 
-        This will return the underlying ID, not the vanity URL.
+    This will return the underlying ID, not the vanity URL.
 
-        :rtype: str
-        """
-        return self.initial_data['metadata']['channelMetadataRenderer']['externalId']
+    :rtype: str
+    """
+    return self.initial_data['metadata']['channelMetadataRenderer']['externalId']
 
-    @property
-    def vanity_url(self):
-        """Get the vanity URL of the YouTube channel.
+  @property
+  def vanity_url(self):
+    """Get the vanity URL of the YouTube channel.
 
-        Returns None if it doesn't exist.
+    Returns None if it doesn't exist.
 
-        :rtype: str
-        """
-        return self.initial_data['metadata']['channelMetadataRenderer'].get('vanityChannelUrl', None)  # noqa:E501
+    :rtype: str
+    """
+    return self.initial_data['metadata']['channelMetadataRenderer'].get('vanityChannelUrl', None)  # noqa:E501
 
-    @property
-    def html(self):
-        """Get the html for the /videos page.
+  @property
+  def html(self):
+    """Get the html for the /videos page.
 
-        :rtype: str
-        """
-        if self._html:
-            return self._html
-        self._html = request.get(self.videos_url)
-        return self._html
+    :rtype: str
+    """
+    if self._html:
+      return self._html
+    self._html = request.get(self.videos_url)
+    return self._html
 
-    @property
-    def playlists_html(self):
-        """Get the html for the /playlists page.
+  @property
+  def playlists_html(self):
+    """Get the html for the /playlists page.
 
-        Currently unused for any functionality.
+    Currently unused for any functionality.
 
-        :rtype: str
-        """
-        if self._playlists_html:
-            return self._playlists_html
-        else:
-            self._playlists_html = request.get(self.playlists_url)
-            return self._playlists_html
+    :rtype: str
+    """
+    if self._playlists_html:
+      return self._playlists_html
+    else:
+      self._playlists_html = request.get(self.playlists_url)
+      return self._playlists_html
 
-    @property
-    def community_html(self):
-        """Get the html for the /community page.
+  @property
+  def community_html(self):
+    """Get the html for the /community page.
 
-        Currently unused for any functionality.
+    Currently unused for any functionality.
 
-        :rtype: str
-        """
-        if self._community_html:
-            return self._community_html
-        else:
-            self._community_html = request.get(self.community_url)
-            return self._community_html
+    :rtype: str
+    """
+    if self._community_html:
+      return self._community_html
+    else:
+      self._community_html = request.get(self.community_url)
+      return self._community_html
 
-    @property
-    def featured_channels_html(self):
-        """Get the html for the /channels page.
+  @property
+  def featured_channels_html(self):
+    """Get the html for the /channels page.
 
-        Currently unused for any functionality.
+    Currently unused for any functionality.
 
-        :rtype: str
-        """
-        if self._featured_channels_html:
-            return self._featured_channels_html
-        else:
-            self._featured_channels_html = request.get(self.featured_channels_url)
-            return self._featured_channels_html
+    :rtype: str
+    """
+    if self._featured_channels_html:
+      return self._featured_channels_html
+    else:
+      self._featured_channels_html = request.get(self.featured_channels_url)
+      return self._featured_channels_html
 
-    @property
-    def about_html(self):
-        """Get the html for the /about page.
+  @property
+  def about_html(self):
+    """Get the html for the /about page.
 
-        Currently unused for any functionality.
+    Currently unused for any functionality.
 
-        :rtype: str
-        """
-        if self._about_html:
-            return self._about_html
-        else:
-            self._about_html = request.get(self.about_url)
-            return self._about_html
+    :rtype: str
+    """
+    if self._about_html:
+      return self._about_html
+    else:
+      self._about_html = request.get(self.about_url)
+      return self._about_html
 
-    @staticmethod
-    def _extract_videos(raw_json: str) -> Tuple[List[str], Optional[str]]:
-        """Extracts videos from a raw json page
+  @staticmethod
+  def _extract_videos(raw_json: str) -> Tuple[List[str], Optional[str]]:
+    """Extracts videos from a raw json page
 
-        :param str raw_json: Input json extracted from the page or the last
-            server response
-        :rtype: Tuple[List[str], Optional[str]]
-        :returns: Tuple containing a list of up to 100 video watch ids and
-            a continuation token, if more videos are available
-        """
-        initial_data = json.loads(raw_json)
-        # this is the json tree structure, if the json was extracted from
-        # html
+    :param str raw_json: Input json extracted from the page or the last
+        server response
+    :rtype: Tuple[List[str], Optional[str]]
+    :returns: Tuple containing a list of up to 100 video watch ids and
+        a continuation token, if more videos are available
+    """
+    initial_data = json.loads(raw_json)
+    # this is the json tree structure, if the json was extracted from
+    # html
+    try:
+      videos = initial_data["contents"][
+          "twoColumnBrowseResultsRenderer"][
+          "tabs"][1]["tabRenderer"]["content"][
+          "sectionListRenderer"]["contents"][0][
+          "itemSectionRenderer"]["contents"][0][
+          "gridRenderer"]["items"]
+    except (KeyError, IndexError, TypeError):
+      try:
+        # this is the json tree structure, if the json was directly sent
+        # by the server in a continuation response
+        important_content = initial_data[1]['response']['onResponseReceivedActions'][
+            0
+        ]['appendContinuationItemsAction']['continuationItems']
+        videos = important_content
+      except (KeyError, IndexError, TypeError):
         try:
-            videos = initial_data["contents"][
-                "twoColumnBrowseResultsRenderer"][
-                "tabs"][1]["tabRenderer"]["content"][
-                "sectionListRenderer"]["contents"][0][
-                "itemSectionRenderer"]["contents"][0][
-                "gridRenderer"]["items"]
-        except (KeyError, IndexError, TypeError):
-            try:
-                # this is the json tree structure, if the json was directly sent
-                # by the server in a continuation response
-                important_content = initial_data[1]['response']['onResponseReceivedActions'][
-                    0
-                ]['appendContinuationItemsAction']['continuationItems']
-                videos = important_content
-            except (KeyError, IndexError, TypeError):
-                try:
-                    # this is the json tree structure, if the json was directly sent
-                    # by the server in a continuation response
-                    # no longer a list and no longer has the "response" key
-                    important_content = initial_data['onResponseReceivedActions'][0][
-                        'appendContinuationItemsAction']['continuationItems']
-                    videos = important_content
-                except (KeyError, IndexError, TypeError) as p:
-                    logger.info(p)
-                    return [], None
+          # this is the json tree structure, if the json was directly sent
+          # by the server in a continuation response
+          # no longer a list and no longer has the "response" key
+          important_content = initial_data['onResponseReceivedActions'][0][
+              'appendContinuationItemsAction']['continuationItems']
+          videos = important_content
+        except (KeyError, IndexError, TypeError) as p:
+          logger.info(p)
+          return [], None
 
-        try:
-            continuation = videos[-1]['continuationItemRenderer'][
-                'continuationEndpoint'
-            ]['continuationCommand']['token']
-            videos = videos[:-1]
-        except (KeyError, IndexError):
-            # if there is an error, no continuation is available
-            continuation = None
+    try:
+      continuation = videos[-1]['continuationItemRenderer'][
+          'continuationEndpoint'
+      ]['continuationCommand']['token']
+      videos = videos[:-1]
+    except (KeyError, IndexError):
+      # if there is an error, no continuation is available
+      continuation = None
 
-        # remove duplicates
-        return (
-            uniqueify(
-                list(
-                    # only extract the video ids from the video data
-                    map(
-                        lambda x: (
-                            f"/watch?v="
-                            f"{x['gridVideoRenderer']['videoId']}"
-                        ),
-                        videos
-                    )
-                ),
+    # remove duplicates
+    return (
+        uniqueify(
+            list(
+                # only extract the video ids from the video data
+                map(
+                    lambda x: (
+                        f"/watch?v="
+                        f"{x['gridVideoRenderer']['videoId']}"
+                    ),
+                    videos
+                )
             ),
-            continuation,
-        )
+        ),
+        continuation,
+    )
 
-    @staticmethod
-    def _extract_playlists(raw_json: str) -> Tuple[List[str], Optional[str]]:
-        """Extracts playlists from a raw json page
+  @staticmethod
+  def _extract_playlists(raw_json: str) -> Tuple[List[str], Optional[str]]:
+    """Extracts playlists from a raw json page
 
-        :param str raw_json: Input json extracted from the page or the last
-            server response
-        :rtype: Tuple[List[str], Optional[str]]
-        :returns: Tuple containing a list of up to 100 video watch ids and
-            a continuation token, if more videos are available
-        """
-        initial_data = json.loads(raw_json)
+    :param str raw_json: Input json extracted from the page or the last
+        server response
+    :rtype: Tuple[List[str], Optional[str]]
+    :returns: Tuple containing a list of up to 100 video watch ids and
+        a continuation token, if more videos are available
+    """
+    initial_data = json.loads(raw_json)
 
-        # this is the json tree structure, if the json was extracted from
-        # html
-        try:
-            playlists = initial_data["contents"][
-                "twoColumnBrowseResultsRenderer"][
-                "tabs"][2]["tabRenderer"]["content"][
-                "sectionListRenderer"]["contents"][0][
-                "itemSectionRenderer"]["contents"][0][
-                'shelfRenderer']["content"][
-                'horizontalListRenderer']["items"]
-        except (KeyError, IndexError, TypeError):
-            playlists = []
+    # this is the json tree structure, if the json was extracted from
+    # html
+    try:
+      playlists = initial_data["contents"][
+          "twoColumnBrowseResultsRenderer"][
+          "tabs"][3]["tabRenderer"]["content"][
+          "sectionListRenderer"]["contents"][0][
+          "itemSectionRenderer"]["contents"][0][
+          'shelfRenderer']["content"][
+          'horizontalListRenderer']["items"]
+    except (KeyError, IndexError, TypeError):
+      print("Error extracting playlists")
+      playlists = []
 
-        continuation = None
-        # remove duplicates
-        return (
-            uniqueify(
-                list(
-                    # only extract the video ids from the video data
-                    map(
-                        lambda x: (
-                            f"/playlist?list="
-                            f"{x['gridPlaylistRenderer']['playlistId']}"
-                        ),
-                        playlists
-                    )
-                ),
+    continuation = None
+    # remove duplicates
+    return (
+        uniqueify(
+            list(
+                # only extract the video ids from the video data
+                map(
+                    lambda x: (
+                        f"/playlist?list="
+                        f"{x['gridPlaylistRenderer']['playlistId']}"
+                    ),
+                    playlists
+                )
             ),
-            continuation,
-        )
+        ),
+        continuation,
+    )
 
-    @property
-    def playlists(self) -> Iterable[Playlist]:
-        """Yields Playlist objects of playlists in this channel
+  @property
+  def playlists(self) -> Iterable[Playlist]:
+    """Yields Playlist objects of playlists in this channel
 
-        :rtype: List[Playlist]
-        :returns: List of Playlist
-        """
-        return DeferredGeneratorList(self.playlist_generator())
+    :rtype: List[Playlist]
+    :returns: List of Playlist
+    """
+    return DeferredGeneratorList(self.playlist_generator())
 
-    def playlist_generator(self):
-        for url in self.playlist_urls:
-            yield Playlist(url)
+  def playlist_generator(self):
+    for url in self.playlist_urls:
+      yield Playlist(url)
 
-    def playlist_url_generator(self):
-        """Generator that yields video URLs.
+  def playlist_url_generator(self):
+    """Generator that yields video URLs.
 
-        :Yields: Video URLs
-        """
-        for page in self._paginate_playlists():
-            for playlist in page:
-                yield self._playlist_url(playlist)
+    :Yields: Video URLs
+    """
+    for page in self._paginate_playlists():
+      for playlist in page:
+        yield self._playlist_url(playlist)
 
-    def _paginate_playlists(
-        self, until_watch_id: Optional[str] = None
-    ) -> Iterable[List[str]]:
-        """Parse the playlist links from the page source, yields the /watch?v=
-        part from video link
+  def _paginate_playlists(
+      self, until_watch_id: Optional[str] = None
+  ) -> Iterable[List[str]]:
+    """Parse the playlist links from the page source, yields the /watch?v=
+    part from video link
 
-        :param until_watch_id Optional[str]: YouTube Video watch id until
-            which the playlist should be read.
+    :param until_watch_id Optional[str]: YouTube Video watch id until
+        which the playlist should be read.
 
-        :rtype: Iterable[List[str]]
-        :returns: Iterable of lists of YouTube playlist ids
-        """
-        playlist_urls, continuation = self._extract_playlists(
-            json.dumps(extract.initial_data(self.playlists_html))
-        )
-        yield playlist_urls
+    :rtype: Iterable[List[str]]
+    :returns: Iterable of lists of YouTube playlist ids
+    """
+    playlist_urls, continuation = self._extract_playlists(
+        json.dumps(extract.initial_data(self.playlists_html))
+    )
+    yield playlist_urls
 
-    @staticmethod
-    def _playlist_url(playlist_path: str):
-        return f"https://www.youtube.com{playlist_path}"
+  @staticmethod
+  def _playlist_url(playlist_path: str):
+    return f"https://www.youtube.com{playlist_path}"
 
-    @property  # type: ignore
-    @cache
-    def playlist_urls(self) -> DeferredGeneratorList:
-        """Complete links of all the playlists in channel
+  @property  # type: ignore
+  @cache
+  def playlist_urls(self) -> DeferredGeneratorList:
+    """Complete links of all the playlists in channel
 
-        :rtype: List[str]
-        :returns: List of playlist URLs
-        """
-        return DeferredGeneratorList(self.playlist_url_generator())
+    :rtype: List[str]
+    :returns: List of playlist URLs
+    """
+    return DeferredGeneratorList(self.playlist_url_generator())
